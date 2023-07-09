02088e4e690dc55922f30595b2219f742088c175
artreysan <70738181+beartree-ars@users.noreply.github.com>
2023-07-09 17:19:37 -0600

    chat finished

--- usellm-tutorial/src/App.jsx
+++ usellm-tutorial/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
 import useLLM  from 'usellm'; // Asegúrate de que el nombre del módulo es correcto y está instalado en tu proyecto
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState('');
   const llm = useLLM({
     serviceUrl: 'https://usellm.org/api/llm'
   });
@@ -11,13 +13,20 @@ function App() {
       <button
         onClick={async () => {
           const data = await llm.chat({
-            messages: [{ role: "user", content: "Hello, how are you?" }]
+            messages: [{ role: "user", content: "dame una lista de lenguajes faciles de aprender" }],
+            stream: true,
+            onStream: ({message}) =>  setResult(message.content)
           });
           console.log(data);
         }}
       >
         Chat
       </button>
+      <div style={{whiteSpace : "pre-wrap"}}>
+        <span>
+          {result}
+        </span>
+      </div>
     </div>
   );
 }
